e8d705e8ea2c797f599ce0f5d933d378950507fb
Richard Dube <richardddube@gmail.com>
2025-07-30 11:10:21 -0400

    use gpu and save shap values

--- bdt_pid/bdt_pid.py
+++ bdt_pid/bdt_pid.py
@@ -19,6 +19,7 @@ save_path = '/home/rdube/PID_paper/pid_mlp/bdt_plots/'
 neutral_ptype = [22,130,2112]
 neutral = xgb.Booster()
 neutral.load_model('/home/rdube/PID_paper/results/neutral_model_bdt.json')
+neutral.set_param({"device":"cuda"})
 
 test_neutral = load_file("neutral","Test", neutral_ptype.index)
 pred_ptype_neut, true_ptype_neut = make_predictions(neutral, test_neutral, match_hypotheses=False)
@@ -27,9 +28,10 @@ pred_ptype_neut, true_ptype_neut = make_predictions(neutral, test_neutral, match
 np.save(save_path + 'neutral_bdt_true.npy', true_ptype_neut)
 np.save(save_path + 'neutral_bdt_pred.npy',  pred_ptype_neut)
 
-neutral_explainer = ShapExplainer(neutral, test_neutral, None, classes=["gamma","KL","n"])
+neutral_explainer = ShapExplainer(neutral, test_neutral, None, classes=["gamma","KL","n"], n_explain_per_particle=1000)
 neutral_explainer.calculate_shap()
-neutral_explainer.plot_summary(5)
+neutral_explainer.plot_summary(10)
+neutral_explainer.save_shap_values("/home/rdube/PID_paper/pid_mlp/bdt_plots/neutral_shap.npy")
 
 ### Charged Particle PID
 
@@ -40,6 +42,7 @@ charged_ptype = {2212:0, 321:1, -13:2, 211:2, -11:3, -2212:4,-321:5,13:6, -211:6
 
 charged = xgb.Booster()
 charged.load_model('/home/rdube/PID_paper/results/charged_model_bdt.json')
+charged.set_param({"device":"cuda"})
 ## Import Data
 
 test_charged = load_file("charged","Test", charged_ptype)
@@ -54,7 +57,8 @@ np.save(save_path + 'charged_bdt_pred.npy',  pred_ptype_char)
 
 ### pick the hypothesis with the highest confidence for neutral particles
 
-charged_explainer = ShapExplainer(charged, test_charged, None, classes=["p+","K+","mu+pi+","e+","p-","K-","mu-pi-","e-"])
+charged_explainer = ShapExplainer(charged, test_charged, None, classes=["p+","K+","mu+pi+","e+","p-","K-","mu-pi-","e-"], n_explain_per_particle=1000)
 charged_explainer.calculate_shap()
-charged_explainer.plot_summary(5)
+charged_explainer.plot_summary(10)
+charged_explainer.save_shap_values("/home/rdube/PID_paper/pid_mlp/bdt_plots/charged_shap.npy")
 
