db2d197e6e2e7d0a1afb2c666f518af828fa67fb
Eric Habjan <erichabjan@gmail.com>
2025-08-05 19:22:18 -0400

    minor updates

--- mlp_pid/neutral_mlp.py
+++ mlp_pid/neutral_mlp.py
@@ -103,10 +103,10 @@ tf_val = tf_val.prefetch(tf.data.AUTOTUNE)
 def model_func(hp):
     model = tf.keras.models.Sequential()
 
-    for i in range(1, hp.Int(f"layers", min_value=1, max_value=7)):
-        model.add(tf.keras.layers.Dense(units=hp.Int(f"neurons_{i}", min_value=100, max_value=600), activation='relu', kernel_regularizer='l1_l2'))
+    #for i in range(1, hp.Int(f"layers", min_value=1, max_value=4)):
+     #   model.add(tf.keras.layers.Dense(units=hp.Int(f"neurons_{i}", min_value=100, max_value=600), activation='relu', kernel_regularizer='l1_l2'))
     
-    #model.add(tf.keras.layers.Dense(units=hp.Int(f"neurons", min_value=50, max_value=1000), activation='relu', kernel_regularizer='l1_l2'))
+    model.add(tf.keras.layers.Dense(units=hp.Int(f"neurons", min_value=50, max_value=1000), activation='relu', kernel_regularizer='l1_l2'))
     
     model.add(tf.keras.layers.Dense(len(ptype), activation = 'softmax'))
 
@@ -125,7 +125,7 @@ tuner = kt.Hyperband(model_func,
                      objective=kt.Objective("val_event_acc", direction="max"), 
                      max_epochs = epochs, 
                      factor=3,
-                     hyperband_iterations=3, 
+                     hyperband_iterations=1, 
                      directory='neutral_model_dir', 
                      project_name='intro_to_kt')
 
@@ -144,14 +144,13 @@ best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
 ### Train model
 
 model = tuner.hypermodel.build(best_hps)
-model.fit(x=trainx, 
-          y=trainy, 
+model.fit(tf_train, 
           epochs= epochs, 
-          validation_data=(valx, valy), 
+          validation_data=tf_val, 
           callbacks = [ev_callback, early_stop], 
           verbose = 1)
 
 ### Save Model
 
-suffix = '_paper'
+suffix = '_paper_1_layer'
 model.save('/projects/mccleary_group/habjan.e/PID_code/Main_analysis/NN_models/Neutral_model' + suffix + '.keras')
