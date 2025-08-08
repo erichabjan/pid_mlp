fixed shap calculation for mlp

bdt_pid/misc.py
@@ -65,7 +65,7 @@ def violin(shap_values, ax, features=None,max_display=5,color="coolwarm",layered
     ax.axvline(x=0, color="#999999", zorder=-1)
     num_x_points = 200
     bins = (np.linspace(0, features.shape[0], layered_violin_max_num_bins + 1).round(0).astype("int"))  # the indices of the feature data corresponding to each bin
-    shap_min, shap_max = -10,10
+    shap_min, shap_max = np.quantile(shap_values, [0.001, 0.999]) * 1.5
     x_points = np.linspace(shap_min, shap_max, num_x_points)
     # loop through each feature and plot:
     for pos, ind in enumerate(feature_order):

bdt_pid/shap_calculation.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import shap
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow.keras import Model, layers
 
 class ShapExplainer:
     def __init__(self, model, data_to_explain, background_data, classes = [], n_background_per_particle=200, n_explain_per_particle=1000):
@@ -20,7 +21,24 @@ class ShapExplainer:
         if isinstance(self.model, tf.keras.Model):
             self.model_type = "MLP"
             self.background_data = background_data
-            self.explainer = shap.DeepExplainer(model, self.background_data)
+
+            last_dense   = self.model.layers[-1]
+            penult_output = self.model.layers[-2].output
+
+            W, b = last_dense.get_weights()
+
+            logits_tensor = layers.Dense(
+                units       = last_dense.units,
+                activation  = None,
+                weights     = [W, b],
+                name        = last_dense.name + "_logits"
+            )(penult_output)
+
+            logits_model = Model(inputs=self.model.input,
+                                 outputs=logits_tensor)
+            self.explainer = shap.DeepExplainer(logits_model,
+                                                self.background_data)
+
         elif isinstance(self.model, xgb.Booster):
             self.model_type = "BDT"
             self.explainer = shap.TreeExplainer(model)