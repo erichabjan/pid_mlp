55e2946e8525e24a5139948f25d5b679159b7b9d
Richard Dube <richardddube@gmail.com>
2025-08-10 09:41:19 -0400

    Updated to show nans in violin plot

--- bdt_pid/misc.py
+++ bdt_pid/misc.py
@@ -55,59 +55,99 @@ def make_predictions(model, data_raw,match_hypotheses=True, confidence_cut = 0.4
         true_ptypes_final = true_ptypes
     return preds_final, true_ptypes_final
 
-## This is a modified version of the violin plot from the SHAP library, with added flexibility for using axes and simplified to use the formatting in this paper
-def violin(shap_values, ax, features=None,max_display=5,color="coolwarm",layered_violin_max_num_bins=20,x_label="SHAP Values",show_label_names=True):
+def feature_order(bdt_shap_vals, mlp_shap_vals, nfeatures=5):
+    mean_bdt = np.average(np.abs(bdt_shap_vals), axis=0)
+    mean_mlp = np.average(np.abs(mlp_shap_vals), axis=0)
+    feature_order = np.argsort((mean_bdt+mean_mlp)/2, axis=0)
+    feature_order = feature_order[-min(nfeatures, len(feature_order)) :]
+    return feature_order
+
+# === MODIFIED VIOLIN FUNCTION START ===
+# This is a modified version of the violin plot from the SHAP library, with added flexibility for using axes and simplified to use the formatting in this paper
+def violin(shap_values, ax, features=None,max_display=5,color="coolwarm",layered_violin_max_num_bins=10,x_label="SHAP Values",show_label_names=True, feature_order=[]):
     feature_names = features.columns
     features = features.to_numpy()
     ax.set_xscale("symlog")
-    feature_order = np.argsort(np.sum(np.abs(shap_values), axis=0))
-    feature_order = feature_order[-min(max_display, len(feature_order)) :]
     ax.axvline(x=0, color="#999999", zorder=-1)
+    
     num_x_points = 200
-    bins = (np.linspace(0, features.shape[0], layered_violin_max_num_bins + 1).round(0).astype("int"))  # the indices of the feature data corresponding to each bin
-    shap_min, shap_max = np.quantile(shap_values, [0.001, 0.999]) * 1.5
+    # Use nanquantile to be safe from NaNs in SHAP values
+    shap_min, shap_max = np.nanquantile(shap_values, [0.001, 0.999]) * 1.5
+    if np.isnan(shap_min): shap_min = -1
+    if np.isnan(shap_max): shap_max = 1
     x_points = np.linspace(shap_min, shap_max, num_x_points)
+    
     # loop through each feature and plot:
     for pos, ind in enumerate(feature_order):
-        # decide how to handle: if #unique < layered_violin_max_num_bins then split by unique value, otherwise use bins/percentiles.
-        # to keep simpler code, in the case of uniques, we just adjust the bins to align with the unique counts.
-        feature = features[:, ind]
-        unique, counts = np.unique(feature, return_counts=True)
-        if unique.shape[0] <= layered_violin_max_num_bins:
-            order = np.argsort(unique)
-            thesebins = np.cumsum(counts[order])
-            thesebins = np.insert(thesebins, 0, 0)
-        else:
-            thesebins = bins
-        nbins = thesebins.shape[0] - 1
-        # order the feature data so we can apply percentiling
-        order = np.argsort(feature)
-        # x axis is located at y0 = pos, with pos being there for offset
-        # y0 = np.ones(num_x_points) * pos
-        # calculate kdes:
-        ys = np.zeros((nbins, num_x_points))
-        for i in range(nbins):
-            # get shap values in this bin:
-            shaps = shap_values[order[thesebins[i] : thesebins[i + 1]], ind]
-            # if there's only one element, then we can't
-            # save kde of them: note that we add a tiny bit of gaussian noise to avoid singular matrix errors
-            ys[i, :] = gaussian_kde(shaps + np.random.normal(loc=0, scale=0.001, size=shaps.shape[0]))(x_points)
-            # scale it up so that the 'size' of each y represents the size of the bin. For continuous data this will
-            # do nothing, but when we've gone with the unique option, this will matter - e.g. if 99% are male and 1%
-            # female, we want the 1% to appear a lot smaller.
-            size = thesebins[i + 1] - thesebins[i]
-            bin_size_if_even = features.shape[0] / nbins
-            relative_bin_size = size / bin_size_if_even
-            ys[i, :] *= relative_bin_size
-        # now plot 'em. We don't plot the individual strips, as this can leave whitespace between them.
-        # instead, we plot the full kde, then remove outer strip and plot over it, etc., to ensure no
-        # whitespace
-        ys = np.cumsum(ys, axis=0)
+        # 1. DATA SEPARATION
+        feature_all = features[:, ind]
+        shap_values_all = shap_values[:, ind]
+        nan_mask = np.isnan(feature_all)
+        
+        nan_shaps = shap_values_all[nan_mask]
+        
+        feature_non_nan = feature_all[~nan_mask]
+        shap_values_non_nan = shap_values_all[~nan_mask]
+        
+        # --- KDE Calculation ---
+        all_ys = []
+        colors = []
+        
+        # 2. Handle NaN data first (will be the first layer, colored gray)
+        if len(nan_shaps) > 1:
+            nan_kde = gaussian_kde(nan_shaps + np.random.normal(loc=0, scale=0.001, size=nan_shaps.shape[0]))(x_points)
+            nan_kde *= len(nan_shaps) # Scale by number of points
+            mcolors.to_rgba('gray') # Assign gray color for NaNs
+            
+        # 3. Handle non-NaN data (colored layers)
+        if len(shap_values_non_nan) > 1:
+            # Decide binning strategy for non-NaN data
+            unique, counts = np.unique(feature_non_nan, return_counts=True)
+            if unique.shape[0] > 1 and unique.shape[0] <= layered_violin_max_num_bins:
+                order = np.argsort(unique)
+                thesebins = np.cumsum(counts[order])
+                thesebins = np.insert(thesebins, 0, 0)
+            else:
+                thesebins = (np.linspace(0, feature_non_nan.shape[0], layered_violin_max_num_bins + 1).round(0).astype("int"))
+            
+            nbins = thesebins.shape[0] - 1
+            if nbins < 1: nbins = 1
+
+            # Order the non-NaN feature data so we can apply percentiling
+            order = np.argsort(feature_non_nan)
+            
+            cmap = plt.get_cmap(color)
+            
+            # Calculate KDE for each bin
+            for i in range(nbins):
+                shaps_in_bin = shap_values_non_nan[order[thesebins[i] : thesebins[i + 1]]]
+                if shaps_in_bin.shape[0] < 2:
+                    continue # Skip bins with less than 2 points for KDE
+                
+                kde = gaussian_kde(shaps_in_bin + np.random.normal(loc=0, scale=0.001, size=shaps_in_bin.shape[0]))(x_points)
+                kde *= len(shaps_in_bin) # Scale KDE by number of points in the bin
+                
+                all_ys.append(kde)
+                colors.append(cmap(i / (nbins - 1) if nbins > 1 else 0.5))
+
+        # 4. PLOTTING
+        if not all_ys: # If no data to plot for this feature, skip
+            continue
+
+        all_ys = np.array(all_ys)
+        all_ys_cumulative = np.cumsum(all_ys, axis=0)
+        
+        # Define violin plot width and find scale factor
         width = 0.8
-        scale = ys.max() * 2 / width  # 2 is here as we plot both sides of x axis
-        for i in range(nbins - 1, -1, -1):
-            y = ys[i, :] / scale
-            c = plt.get_cmap(color)(i / (nbins - 1))
+        if np.max(all_ys_cumulative) > 0:
+            scale = np.max(all_ys_cumulative) * 2 / width
+        else:
+            scale = 1
+        
+        # Plot each layer, from the outside in (i.e., largest cumulative area first)
+        for i in range(len(all_ys) - 1, -1, -1):
+            y = all_ys_cumulative[i, :] / scale
+            c = colors[i]
             ax.fill_between(x_points, pos - y, pos + y, facecolor=c, edgecolor="face")
     ax.set_xlim(shap_min, shap_max)
     ax.xaxis.set_ticks_position("bottom")
@@ -128,26 +168,31 @@ def violin(shap_values, ax, features=None,max_display=5,color="coolwarm",layered
 def SHAP_plots(mlp_shap_vals, mlp_data, bdt_shap_vals, bdt_data, charge):
     if charge == "pos":
         nClasses = 4
-        classes = {0:r'$p$', 1:r'$K^{+}$', 2:r'$\pi^{+}$ | $\mu^{+}$', 3:r'$e^{+}$'}
+        bdt_indices = [0,1,2,3]
+        mlp_indices = [0,2,6,5]
+        classes = [r'$p$',r'$K^{+}$',r'$\pi^{+}$ | $\mu^{+}$', r'$e^{+}$']
         index_offset=0
     elif charge=="neg":
         nClasses = 4
-        index_offset = 4
-        classes = {4:r'$\bar{p}$', 5:r'$K^{-}$', 6:r'$\pi^{-}$ | $\mu^{-}$', 7:r'$e^{-}$'}
+        bdt_indices = [4,5,6,7]
+        mlp_indices = [1,3,7,4]
+        classes = [r'$\bar{p}$', r'$K^{-}$', r'$\pi^{-}$ | $\mu^{-}$', r'$e^{-}$']
     else:
         nClasses = 3
-        classes = {0:r'$\gamma$', 1:r'$K_{L}^{0}$', 2:r'$n$'}
-        index_offset=0
+        bdt_indices = [0,1,2]
+        mlp_indices = [0,1,2]
+        classes = [r'$\gamma$', r'$K_{L}^{0}$', r'$n$']
     fig, axes = plt.subplots(nrows=nClasses, ncols=2, figsize=(10,11))
-    for i in classes.keys():
-        if i == max(classes.keys()):
+    for i in range(nClasses):
+        if i == nClasses-1:
             BDT_x_label = "BDT SHAP Values"
             MLP_x_label = "MLP SHAP Values"
         else:
             BDT_x_label = ""
             MLP_x_label = ""
-        violin(mlp_shap_vals[i],axes[i-index_offset][0],features=mlp_data,x_label=MLP_x_label)
-        violin(bdt_shap_vals[i],axes[i-index_offset][1],features=bdt_data,x_label=BDT_x_label, show_label_names=False)
+        fo = feature_order(bdt_shap_vals[bdt_indices[i]],mlp_shap_vals[mlp_indices[i]])
+        violin(mlp_shap_vals[mlp_indices[i]],axes[i][0],features=mlp_data,x_label=MLP_x_label, feature_order=fo)
+        violin(bdt_shap_vals[bdt_indices[i]],axes[i][1],features=bdt_data,x_label=BDT_x_label, show_label_names=False, feature_order=fo)
     #plt.tight_layout()
     fig.subplots_adjust(left=0.12,right=0.98,wspace=0.1,hspace=0.3, bottom=0, top=0.95)
     m = cm.ScalarMappable(cmap=plt.get_cmap("coolwarm"))
@@ -171,7 +216,6 @@ def SHAP_plots(mlp_shap_vals, mlp_data, bdt_shap_vals, bdt_data, charge):
     for j in range(nClasses-1,-1,-1):
         leftBox = axes[j][0].get_position()
         fig.text(leftBox.x1+rel_pos_x, leftBox.y1+rel_pos_y, "SHAP Values For: " + classes[index_offset+j], ha='center', va='center', fontsize=13)
-    fig.show()
     fig.savefig("/home/rdube/PID_paper/pid_mlp/bdt_plots/" + charge + "_SHAP.png",dpi=300)
 
 
